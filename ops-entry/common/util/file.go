380eaa80c976f67dd80a818d9a8c17f60ead5efc
lauk <liukuo@kylinos.cn>
2024-08-13 15:31:40 +0800

    optimized the code that gets the name of the stored file

--- ops-entry/common/util/file.go
+++ ops-entry/common/util/file.go
@@ -1,9 +1,16 @@
 package util
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"ops-entry/constValue"
 	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 // IsAllowedExtension 检查扩展名是否在给定的允许列表中
@@ -28,4 +35,44 @@ func CreatePath(path string) error {
 	}
 
 	return nil
+}
+
+func ParseLabels(labelData string) (map[string]string, error) {
+	var labels map[string]string
+	if err := json.Unmarshal([]byte(labelData), &labels); err != nil {
+		logrus.Errorf("error unmarshal lables data:%v", labelData)
+		return nil, errors.New("error unmarshal lables data:" + err.Error())
+	}
+	return labels, nil
+}
+
+// 获取集群配置文件存储名称
+func GetSaveFilename(labelData string, clusterID string) (string, error) {
+	currentUser, err := user.Current()
+	if err != nil {
+		logrus.Errorf("Error getting current user:%v", err)
+		return "", err
+	}
+
+	savePath := filepath.Join(currentUser.HomeDir, constValue.ClusterConfigSavePath, clusterID)
+	err = CreatePath(savePath)
+	if err != nil {
+		logrus.Errorf("Error creating path:%v", err)
+		return "", err
+	}
+
+	filename := clusterID
+	if labelData != "" {
+		labels, err := ParseLabels(labelData)
+		if err != nil {
+			return "", err
+		}
+
+		for key, value := range labels {
+			filename = filename + "-" + key + "-" + value
+		}
+	}
+
+	dst := filepath.Join(savePath, fmt.Sprintf("%s%s", filename, ".yaml"))
+	return dst, nil
 }
